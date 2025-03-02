086829b343263b3767ead13b04030c5cc11c7577
kentr <kent.rattley@gmail.com>
2025-03-02 23:52:38 +1030

    move to block system

---
+++ src/chunk.h
@@ -0,0 +1,208 @@
+#include "header.h"
+
+/*
+Chunk
+- block types [16][16][16]
+
+- mesh 
+
+
+*/
+
+float verticies[6][18] = {
+    {
+        // Front face
+        0.0f, 0.0f, 1.0f,  // Triangle 1
+        1.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,  // Triangle 2
+        1.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f
+    },
+    {
+        // Back face
+        0.0f, 0.0f, 0.0f,  // Triangle 3
+        1.0f, 1.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f,  // Triangle 4
+        0.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f
+    },
+    {
+        // Top face
+        0.0f, 1.0f, 0.0f,  // Triangle 5
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,  // Triangle 6
+        0.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f
+    },
+    {
+        // Bottom face
+        0.0f, 0.0f, 0.0f,  // Triangle 7
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,  // Triangle 8
+        1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f
+    },
+    {
+        // Right face
+        1.0f, 0.0f, 0.0f,  // Triangle 9
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 0.0f,  // Triangle 10
+        1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 1.0f
+    },
+    {
+        // Left face
+        0.0f, 0.0f, 0.0f,  // Triangle 11
+        0.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 0.0f,  // Triangle 12
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 0.0f
+    }
+};
+
+float colours[6][18] = {
+    {
+        // Front face
+        1.0f, 0.0f, 0.0f,  // Triangle 1
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,  // Triangle 2
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f
+    },
+    {
+        // Back face
+        0.0f, 1.0f, 0.0f,  // Triangle 3
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,  // Triangle 4
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    },
+    {
+        // Top face
+        0.0f, 0.0f, 1.0f,  // Triangle 5
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,  // Triangle 6
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f
+    },
+    {
+        // Bottom face
+        1.0f, 1.0f, 0.0f,  // Triangle 7
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,  // Triangle 8
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f
+    },
+    {
+        // Right face
+        0.0f, 1.0f, 1.0f,  // Triangle 9
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,  // Triangle 10
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f
+    },
+    {
+        // Left face
+        1.0f, 0.0f, 1.0f,  // Triangle 11
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,  // Triangle 12
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f
+    }
+};
+
+using std::vector;
+
+
+class Chunk {
+    const int LENGTH = 16;
+    const int WIDTH = 16;
+    const int HEIGHT = 16;
+
+    vector<vector<vector<int>>> blocks;
+
+    glm::vec3 position;
+    std::vector<float> mesh;
+
+public:
+    Chunk() : position(glm::vec3(0, 0, 0)) {
+        blocks = vector<vector<vector<int>>>(LENGTH, vector<vector<int>>(WIDTH, vector<int>(HEIGHT, 0)));
+    }
+
+    Chunk(glm::vec3 position) : position(position) {
+        blocks = vector<vector<vector<int>>>(LENGTH, vector<vector<int>>(WIDTH, vector<int>(HEIGHT, 0)));
+    }
+    
+
+    Chunk(glm::vec3 position, int type) : position(position) {
+        blocks = vector<vector<vector<int>>>(LENGTH, vector<vector<int>>(WIDTH, vector<int>(HEIGHT, type)));
+    }
+
+    vector<vector<vector<int>>> getBlocks(){
+        return blocks;
+    }
+
+    
+    int getBlock(int x, int y, int z){
+        return blocks[x][y][z];
+    }
+
+    void setBlock(int x, int y, int z, int type){
+        blocks[x][y][z] = type;
+    }
+
+
+    std::vector<float> getMesh(){
+        return mesh;
+    }
+
+
+    void createMesh(){
+        mesh.clear();
+
+        // for(int x = 0; x < LENGTH; x++){
+        //     for(int y = 0; y < WIDTH; y++){
+        //         for(int z = 0; z < HEIGHT; z++){
+        //             if(blocks[x][y][z] == 0) continue;
+        //         }
+        //     }
+        // }
+
+        // add a cube to the mesh, all 6 faces
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 3; j++) mesh.push_back(verticies[i][j]);
+            for(int j = 0; j < 3; j++) mesh.push_back(colours[i][j]);
+
+            for(int j = 3; j < 6; j++) mesh.push_back(verticies[i][j]);
+            for(int j = 3; j < 6; j++) mesh.push_back(colours[i][j]);
+            
+            for(int j = 6; j < 9; j++) mesh.push_back(verticies[i][j]);
+            for(int j = 6; j < 9; j++) mesh.push_back(colours[i][j]);
+
+            for(int j = 9; j < 12; j++) mesh.push_back(verticies[i][j]);
+            for(int j = 9; j < 12; j++) mesh.push_back(colours[i][j]);
+
+            for(int j = 12; j < 15; j++) mesh.push_back(verticies[i][j]);
+            for(int j = 12; j < 15; j++) mesh.push_back(colours[i][j]);
+
+            for(int j = 15; j < 18; j++) mesh.push_back(verticies[i][j]);
+            for(int j = 15; j < 18; j++) mesh.push_back(colours[i][j]);
+        }
+    }
+
+
+
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ Last Update: 2/03/2025
 */
 
 #include "header.h"
+#include "camera.h"
+#include "render.h"
+#include "chunk.h"
 
 
 using namespace std;
@@ -16,157 +19,19 @@ void errorCallback(int error, const char* description) {
 
 class GameEngine3D{
 private:
-	Mesh meshCube;
-	//glm::mat4 matProj;	// Matrix that converts from view space to screen space
 	Camera camera = Camera(glm::vec3{0, 0, -5});
 	int windowWidth;
 	int windowHeight;
 	GLFWwindow* window;
-	std::string filename;
-
-
-	string vertexShaderPath = "src/shaders/shader.vert";
-	string fragmentShaderPath = "src/shaders/shader.frag";
-
-	GLuint shaderProgram;
-	GLuint VAO, VBO;
-
-	void shaderInit(){
-		// 1. load the shader files
-		string vertexShaderCode;
-		string fragmentShaderCode;
-
-		try {
-			ifstream vertexShaderStream(vertexShaderPath, ios::in);
-			ifstream fragmentShaderStream(fragmentShaderPath, ios::in);
-
-			if(!vertexShaderStream.is_open()) throw std::runtime_error("Cannot open vertex shader file");
-			if(!fragmentShaderStream.is_open()) throw std::runtime_error("Cannot open fragment shader file");
-
-			stringstream sstr;
-			sstr << vertexShaderStream.rdbuf();
-			vertexShaderCode = sstr.str();
-			vertexShaderStream.close();
-
-			sstr.str("");
-			sstr << fragmentShaderStream.rdbuf();
-			fragmentShaderCode = sstr.str();
-			fragmentShaderStream.close();
-			
-		} catch (exception e){
-			std::cerr << "Error: " << e.what() << std::endl;
-		}
-
-		if (vertexShaderCode.empty() || fragmentShaderCode.empty()) {
-			std::cerr << "Error: Shader code is empty." << std::endl;
-			return;
-		}
-		
-
-		const char * vertexShaderPtr = vertexShaderCode.c_str();
-		const char * fragmentShaderPtr = fragmentShaderCode.c_str();
-
-
-		// 2. compile the shaders
-		int success;
-		// vertex
-		GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertex, 1, &vertexShaderPtr, NULL);
-		glCompileShader(vertex);
-		glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			char infoLog[512];
-			glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-			std::cerr << "Error: Vertex Shader Compilation Failed\n" << infoLog << std::endl;
-		}
-
-
-
-		// fragment
-		GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragment, 1, &fragmentShaderPtr, NULL);
-		glCompileShader(fragment);
-		glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			char infoLog[512];
-			glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-			std::cerr << "Error: Fragment Shader Compilation Failed\n" << infoLog << std::endl;
-		}
-
-		// create shader program
-		shaderProgram = glCreateProgram();
-		glAttachShader(shaderProgram, vertex);
-		glAttachShader(shaderProgram, fragment);
-		glLinkProgram(shaderProgram);
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);	// check sucess
-		if(!success){
-			char infoLog[512];
-			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-			std::cerr << "Error: Shader Program Linking Failed\n" << infoLog << std::endl;
-		}
-
-		// delete shaders - now linked to program, no longer needed
-		glDeleteShader(vertex);
-		glDeleteShader(fragment);
-	}
-
-
-	void createBuffers(){
-
-		// Create Vertex Array Object
-		glGenVertexArrays(1, &VAO);
-		glBindVertexArray(VAO);
-
-		// Create a Vertex Buffer Object and copy the vertex data to it
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, 0, NULL, GL_DYNAMIC_DRAW);
-
-		// define the vertex attribute pointer
-		// for positions - layer 0
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-		glEnableVertexAttribArray(0);
-		// for colors - layer 1
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-		glEnableVertexAttribArray(1);
-
-
-		// Unbind the VAO
-		glBindVertexArray(0);
-	}
-
-
-	bool GraphicsInit(){
-		// Load object file
-		meshCube.LoadFromObjectFile(filename);
-
-		if(meshCube.tris.empty()){
-			std::cerr << "Error: Mesh is empty" << std::endl;
-			throw std::runtime_error("Mesh is empty");
-		} else {
-			cout << "Loaded mesh with " << meshCube.tris.size() << " triangles" << endl;
-		}
-
-		// Create the shader program
-		shaderInit();	
-
-
-		// Create the vertex buffer object
-		createBuffers();
-
-		// Projection Matrix
-		//matProj = glm::perspective(glm::radians(90.0f), (float)windowWidth / (float)windowHeight, 0.1f, 1000.0f);
-		return true;
-	}
-
+	Render render;
+	Chunk chunk;
 
 
 
 public:
-	GameEngine3D(int w, int h, string _filename){
+	GameEngine3D(int w, int h){
 		windowWidth = w;
 		windowHeight = h;
-		filename = _filename;
 
 		// Initialize GLFW
 		if (!glfwInit()) {
@@ -220,75 +85,19 @@ public:
 
 		
 		// Create user resources as part of this thread
-		if (!GraphicsInit()){
+		if (!render.init(windowWidth, windowHeight)){
 			std::cerr << "Failed on user create" << std::endl;
 			exit(-1);
 		}
 
+		chunk.createMesh();
+		cout << chunk.getMesh().size() << endl;
+
 	}
 
-	bool Render(float fElapsedTime){
-		glm::mat4 matProj = glm::perspective(glm::radians(90.0f), (float) windowWidth / (float) windowHeight, 0.1f, 1000.0f);
-		glm::mat4 matWorld = glm::mat4(1.0f);	// Form World Matrix
-
-		// Get view matrix from camera class
-		glm::mat4 matView = camera.matView();	
-
-		// Use the shader program
-		glUseProgram(shaderProgram);
+	
 
 
-		// Pass matrices to the shader
-		GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
-		GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
-		GLint projLoc = glGetUniformLocation(shaderProgram, "projection");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(matWorld));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(matView));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(matProj));
-
-
-		// Prepare vertex and color data
-		std::vector<float> vertexData;
-		vertexData.reserve(meshCube.tris.size() * 6 * 3); // 3 vertices per triangle, 6 floats per vertex (3 for position, 3 for color)
-
-		for (const auto& tri : meshCube.tris) {
-			for (int i = 0; i < 3; ++i) {
-				// Vertex positions
-				vertexData.push_back(tri.p[i].x);
-				vertexData.push_back(tri.p[i].y);
-				vertexData.push_back(tri.p[i].z);
-
-				// Vertex colors (assuming tri.col is a float representing grayscale intensity)
-				vertexData.push_back(tri.col); // Red
-				vertexData.push_back(tri.col); // Green
-				vertexData.push_back(tri.col); // Blue
-			}
-		}
-
-		if(vertexData.empty()){
-			cout << "Vertex Data is empty" << endl;
-			return false;
-		}
-
-		// 5. Update Vertex Buffer Object (VBO) - new data
-		// use method: glBufferSubData not glMapBuffer
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(float), vertexData.data(), GL_DYNAMIC_DRAW);
-
-
-		// 6. render the object
-		glBindVertexArray(VAO);
-		//glDrawArraysInstanced
-		glDrawArrays(GL_TRIANGLES, 0, vertexData.size() / 6);
-		glBindVertexArray(0);
-
-
-		
-
-
-		return true;
-		
-	}
 
 	void Run(){
 		auto tp1 = std::chrono::system_clock::now();
@@ -357,18 +166,15 @@ public:
 
 			// Standard FPS Control scheme, but turn instead of strafe
 			if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) camera.pos = camera.pos + vForward;
-			
 			if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) camera.pos = camera.pos - vForward;
 			
 			//pan camera left
 			if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) camera.pos = camera.pos + vRight;
-			
 			//pan camera right
 			if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) camera.pos = camera.pos - vRight;
 		
 			//move camera up
 			if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) camera.pos.y += 8.0f * fElapsedTime;
-			
 			//move camera down
 			if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) camera.pos.y -= 8.0f * fElapsedTime;
 			
@@ -402,12 +208,9 @@ public:
 			// Enable the vertex array functionality
 			glEnableClientState(GL_VERTEX_ARRAY);
 
-			// Set the background color to white
-			glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
 
 			// render 3d scene
-			Render(fElapsedTime);
+			render.renderData(camera.viewMatrix(), chunk.getMesh());
 
 
 			// Render ImGui ontop
@@ -420,9 +223,6 @@ public:
 			glfwSwapBuffers(window);
 			// Poll for and process events
 			glfwPollEvents();
-
-			
-
 				
 		}
 
@@ -442,7 +242,7 @@ public:
 
 
 int main(){
-	GameEngine3D game(1200, 800, "teapot.obj");
+	GameEngine3D game(1200, 800);
 
 	game.Run();
 
